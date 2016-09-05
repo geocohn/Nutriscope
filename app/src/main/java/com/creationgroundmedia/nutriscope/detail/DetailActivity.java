57f948f122b7ee18a11de083065a9c0a7abaf12f
geocohn <geo@creationgroundmedia.com>
2016-09-05 16:46:40 -0700

    chore: Add Apache 2.0 copyright to all files

--- app/src/main/java/com/creationgroundmedia/nutriscope/detail/DetailActivity.java
+++ app/src/main/java/com/creationgroundmedia/nutriscope/detail/DetailActivity.java
@@ -1,10 +1,25 @@
+/*
+ * Copyright 2016 George Cohn III
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.creationgroundmedia.nutriscope.detail;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.design.widget.CollapsingToolbarLayout;
--- app/src/main/java/com/creationgroundmedia/nutriscope/detail/DetailIngredientsFragment.java
+++ app/src/main/java/com/creationgroundmedia/nutriscope/detail/DetailIngredientsFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 George Cohn III
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.creationgroundmedia.nutriscope.detail;
 
 import android.content.Context;
@@ -75,7 +91,6 @@ public class DetailIngredientsFragment extends Fragment implements LoaderManager
      * @param upc The product UPC
      * @return A new instance of fragment DetailIngredientsFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static DetailIngredientsFragment newInstance(
             long rowId, String name, String upc) {
         DetailIngredientsFragment fragment = new DetailIngredientsFragment();
