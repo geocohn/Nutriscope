4149e913c678205df6437e599983223afa0242d6
geocohn <geo@creationgroundmedia.com>
2016-09-10 08:41:57 -0700

    style: Restore missing import for log

--- app/src/main/java/com/creationgroundmedia/nutriscope/detail/DetailNutritionFragment.java
+++ app/src/main/java/com/creationgroundmedia/nutriscope/detail/DetailNutritionFragment.java
@@ -26,6 +26,7 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
